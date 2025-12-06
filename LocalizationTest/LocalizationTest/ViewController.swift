c5db125623956497394442fbe592f28d086e3cc7
Kostiantyn Kolontai <kkolontay@gmail.com>
2025-12-06 18:11:57 -0500

    project was added

---
+++ LocalizationTest/LocalizationTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocalizationTest
+//
+//  Created by Kostiantyn Kolontai on 2025-12-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ LocalizationTest/LocalizationTestTests/LocalizationTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationTestTests.swift
+//  LocalizationTestTests
+//
+//  Created by Kostiantyn Kolontai on 2025-12-06.
+//
+
+import Testing
+@testable import LocalizationTest
+
+struct LocalizationTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
