007c42d4acb8a01ae55b742b73fa449a57516416
Kostiantyn Kolontai <kkolontay@gmail.com>
2025-12-08 21:20:53 -0500

    working

--- LocalizationTest/LocalizationTest/SwitchViewController.swift
+++ LocalizationTest/LocalizationTest/SwitchViewController.swift
@@ -9,13 +9,24 @@ import UIKit
 
 class SwitchViewController: UIViewController {
   var currentStatusOfSwiftche: Bool = false
+  @IBOutlet var languageSwitch: UISwitch!
+  @IBOutlet weak var languageLabel: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    languageSwitch.isOn = LocalizationManager.shared.currentLanguage == "en"
+    languageLabel.text = LocalizationManager.shared.currentLanguage
   }
 
   @IBAction func switchWasSwitch(_ sender: UISwitch) {
     currentStatusOfSwiftche = sender.isOn
-    print("\(currentStatusOfSwiftche)")
+    if sender.isOn {
+      LocalizationManager.shared.currentLanguage = "en"
+      print("TEST: \("ZtN-df-oph.text".localized)")
+    } else {
+      LocalizationManager.shared.currentLanguage = "fr-CA"
+      print("TEST: \("ZtN-df-oph.text".localized)")
+    }
   }
-  
+
 }
